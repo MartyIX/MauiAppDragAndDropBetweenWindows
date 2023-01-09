68b56d3487644940674fe9d374b6ec236bb358e2
a <a@b.com>
2023-01-09 10:47:47 +0100

    Add support for multiple windows for Mac Catalyst

---
+++ MauiAppDragAndDropBetweenWindows/Platforms/MacCatalyst/SceneDelegate.cs
@@ -0,0 +1,15 @@
+using Foundation;
+using Microsoft.Maui;
+using UIKit;
+
+namespace MauiAppDragAndDropBetweenWindows;
+
+/// <summary>
+/// Scene delegate is responsible for scenes in MacCatalyst.
+/// </summary>
+/// <seealso href="https://learn.microsoft.com/en-gb/dotnet/maui/fundamentals/windows?view=net-maui-7.0#ipados-and-macos-configuration">The class is required for the multi-window support.</seealso>
+/// <seealso href="https://developer.apple.com/documentation/uikit/app_and_environment/scenes">The "scene" concept.</seealso>
+[Register("SceneDelegate")]
+public class SceneDelegate : MauiUISceneDelegate
+{
+}
